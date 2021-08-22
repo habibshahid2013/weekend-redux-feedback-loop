31b9e61d00b15a2e21272505b862955a335ca5a1
Hassan Shahid <habibshahid2013@gmail.com>
2021-08-21 23:12:38 -0500

    changes made to the DB

--- server/routes/survey.routers.js
+++ server/routes/survey.routers.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { Pool } = require('pg');
 const pool = require('../modules/pool')
 const router = express.Router();
 
@@ -8,11 +9,59 @@ const router = express.Router();
 
 //Post Survey 
 
-
 router.post('/', (req,res) => {
-    sqlQuery = `INSERT INTO feedback("feeling", "understanding", "support", "comment")`
 
-})
+    const newSurvey = req.body
+
+    const sqlQuery = `INSERT INTO feedback("feeling", "understanding", "support", "comment")
+                VALUES
+                ($1, $2, $3, $4)`;
+
+    params = [
+
+        newSurvey.feeling,
+        newSurvey.understanding,
+        newSurvey.support,
+        newSurvey.comments
+    ]
+    db.query(sqlQuery, params)
+        .then((result) => {
+            res.sendStatus(201);
+            console.log('POST /api/survey', result);
+
+        })
+        .catch((error) => {
+            console.log('POST /api/survey Error:', error);
+            res.sendStatus(500);
+        });
+});
+
+
+//Get for initating DB
+router.get('/', (req, res) => {
+    const sqlQuery = `SELECT 
+                            id, 
+                            feeling, 
+                            understanding, 
+                            support, 
+                            comments, 
+                            flagged, 
+                            date 
+                        FROM feedback 
+                        ORDER BY id ASC`;
+
+    db.query(sqlQuery)
+        .then((result) => {
+            res.send(result.rows);
+            console.log('GET /api/survey Records:', result.rows.length);
+        })
+        .catch((error) => {
+            console.log('GET /api/survey Error:', error);
+            res.sendStatus(500);
+        });
+});
+
+
 
 
 
